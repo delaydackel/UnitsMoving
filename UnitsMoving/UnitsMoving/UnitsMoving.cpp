a20c3378f2841f9f465f19f3daf67dd97db172c5
Manuel Vetterlein <m.vetterlein@inoxision.de>
2016-11-29 17:05:39 +0100

    second commit

--- UnitsMoving/UnitsMoving/UnitsMoving.cpp
+++ UnitsMoving/UnitsMoving/UnitsMoving.cpp
@@ -25,6 +25,7 @@ Method signature:	double bestTime(array[] start, array[] finish)
 #include <iostream>
 #include <algorithm>
 #include <typeinfo>
+#include <math.h>
 using namespace std;
 
 class UnitsMoving
@@ -35,26 +36,95 @@ private:
 public:
 	int startingPoints[50][3];
 	int finishPoints[50][2];
-	void setPoint(int position,  int (&point)[3], int (&startingPoints)[50][3]);
-	double individualTime(int *start, int *finish);
+	double times[50][1];
+	void setPoint(int position, int(&point)[3], int(&startingPoints)[50][3]);
+	void setPoint(int position, int(&point)[2], int(&finishPoints)[50][2]);
+	void printPoints(double(&points)[50][1]);
+	void printPoints(int(&points)[50][2]);
+	void printPoints(int(&points)[50][3]);
+	void calculateTimes(int(&startingPoints)[50][3], int(&finishPoints)[50][2], double(&times)[50][1]);
+	double getIndividualTime(int (&startingPoint)[3], int (&finishingPoint)[2]);
 	double bestTime();
 };
-
-void UnitsMoving::setPoint(int pos, int (&point)[3], int (&startingPoints)[50][3]) 
+void UnitsMoving::printPoints(int(&points)[50][2])
 {
-
+	for (int i = 0; i < 50; i++)
+	{
+		for (int j = 0; j < 2; j++)
+		{
+			cout << *&points[i][j]<<" ";
+		}
+		cout << endl;
+	}
+	return;
+}
+void UnitsMoving::printPoints(int(&points)[50][3])
+{
+	for (int i = 0; i < 50; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			cout << *&points[i][j] <<" ";
+		}
+		cout << endl;
+	}
+	return;
+}
+void UnitsMoving::printPoints(double(&points)[50][1])
+{
+	for (int i = 0; i < 50; i++)
+	{
+		cout << *&points[i] << " ";
+		cout << endl;
+	}
+	return;
+}
+void UnitsMoving::setPoint(int pos, int(&point)[3], int(&startingPoints)[50][3]) // sets the coordinates of a point to those of the input point 
+{
 	for (int i = 0; i < 3; i++)
 	{
 		startingPoints[pos][i] = point[i];
 	}
 	return;
 }
+void UnitsMoving::setPoint(int pos, int(&point)[2], int(&finishPoints)[50][2]) // sets the coordinates of a point to those of the input point 
+{
+	for (int i = 0; i < 2; i++)
+	{
+		finishPoints[pos][i] = point[i];
+	}
+	return;
+}
 
-double UnitsMoving::individualTime(int *start, int *finish)
+double UnitsMoving::getIndividualTime(int (&startingPoint)[3], int(&finishingPoint)[2])
 {
 	double distance = 0;
-	cout << start;
-	return 0.0;
+	double time = 0;
+	int startX = *(&startingPoint)[0];
+	int startY = *(&startingPoint)[1];
+	int velocity = *(&startingPoint)[2];
+	int finishX = *(&finishingPoint)[0];
+	int finishY = *(&finishingPoint)[1];
+	double distanceX = finishX-startX;
+	double distanceY = finishY - startY;
+	distance = sqrt(exp(distanceX) + exp(distanceY));
+	if (velocity !=0)
+	{
+		time = distance / velocity;
+	}
+	else{
+		time = -1;
+	}
+
+	return time;
+}
+void UnitsMoving::calculateTimes(int(&startingPoints)[50][3], int(&finishPoints)[50][2], double(&times)[50][1]) 
+{
+	for (int i = 0; i < 50; i++)
+	{
+		*(&times)[i][0]=getIndividualTime((&startingPoints)[i][3],(&finishPoints)[i][2]);
+	}
+
 }
 double UnitsMoving::bestTime()
 {
@@ -66,10 +136,24 @@ int main()
 {
 	int blub=0;
 	UnitsMoving units{};
-	int point[3] = {1,1,1};
-	cout << typeid(point).name()<<endl;
-	units.setPoint(0, point, units.startingPoints);
-	cout << units.startingPoints;
+	int start00[3] = {1,1,1};
+	int start01[3] = { 0,1,1 };
+	int start02[3] = { 5,7,3 };
+	int finish00[2] = { 1,1 };
+	int finish01[2] = { 1,1 };
+	int finish02[2] = { 1,1 };
+
+	//cout << typeid(point01).name()<<endl;
+	units.setPoint(0, start00, units.startingPoints);
+	units.setPoint(1, start01, units.startingPoints);
+	units.setPoint(2, start02, units.startingPoints);
+	units.setPoint(0, finish00, units.finishPoints);
+	units.setPoint(1, finish01, units.finishPoints);
+	units.setPoint(2, finish02, units.finishPoints);
+	units.calculateTimes(units.startingPoints , units.finishPoints, units.times);
+	units.printPoints(units.times);
+	//units.printPoints(units.startingPoints);
+	//units.printPoints(units.finishPoints);
 	cin >> blub;
     return 0;
 }
